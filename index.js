487321188e700cd2f8b16fdb1e6dbb7bd95ea714
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-05-16 16:34:29 +0300

    Created game

---
+++ index.js
@@ -0,0 +1,24 @@
+refs = {
+    dino: document.getElementById('dino'),
+    cactus: document.getElementById('cactus'),
+};
+
+document.addEventListener('keydown', () => {
+    if (!refs.dino.classList.contains('jump')) {
+        refs.dino.classList.add('jump')
+    }
+
+    setTimeout(() => {
+        refs.dino.classList.remove('jump')
+    }, 300)
+});
+
+let isAlive = setInterval(() => {
+    let dinoCoordinates = parseInt(window.getComputedStyle(refs.dino).getPropertyValue('top'));
+    let cactusCoordinates = parseInt(window.getComputedStyle(refs.cactus).getPropertyValue('left'));
+
+    if (cactusCoordinates < 50 && cactusCoordinates > 0 && dinoCoordinates >= 140) {
+        alert('GAME OVER!')
+    }
+}, 10);
+
